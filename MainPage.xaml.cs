5c10c96fe81fd39ba6e45b8c98a669cae6520b7c
megatony <akinakgul@gmail.com>
2014-01-18 03:24:57 +0200

    Final

---
+++ MainPage.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using housemd.Resources;
+using housemd.ViewModels;
+using System.IO.IsolatedStorage;
+using Microsoft.Phone.Tasks;
+
+namespace housemd
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            OylamaDenetleyici(null, null);
+
+            InitializeComponent();
+
+            // Set the data context of the LongListSelector control to the sample data
+            DataContext = App.ViewModel;
+
+            // Sample code to localize the ApplicationBar
+            BuildLocalizedApplicationBar();
+        }
+
+        // Load data for the ViewModel Items
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+        private void OylamaDenetleyici(object sender, LaunchingEventArgs e)
+        {
+            int AcilisSayisi = 0;
+            if (IsolatedStorageSettings.ApplicationSettings.Contains("ProgramAcildi"))
+            {
+                AcilisSayisi = (int)IsolatedStorageSettings.ApplicationSettings["ProgramAcildi"];
+            }
+            else
+            {
+                IsolatedStorageSettings.ApplicationSettings["ProgramAcildi"] = 0;
+            }
+
+            AcilisSayisi++;
+
+            IsolatedStorageSettings.ApplicationSettings["ProgramAcildi"] = AcilisSayisi;
+            if (AcilisSayisi == 5)
+            {
+                var returnvalue = MessageBox.Show("if you liked my application, would you like to tell me that?", "ooops! give me a second", MessageBoxButton.OKCancel);
+                if (returnvalue == MessageBoxResult.OK)
+                {
+                    var marketplaceReviewTask = new MarketplaceReviewTask();
+                    marketplaceReviewTask.Show();
+                }
+            }
+        }
+
+        // Handle selection changed on LongListSelector
+        private void MainLongListSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // If selected item is null (no selection) do nothing
+            if (MainLongListSelector.SelectedItem == null)
+                return;
+
+            // Navigate to the new page
+            NavigationService.Navigate(new Uri("/DetailsPage.xaml?selectedItem=" + (MainLongListSelector.SelectedItem as ItemViewModel).ID, UriKind.Relative));
+
+            // Reset selected item to null (no selection)
+            MainLongListSelector.SelectedItem = null;
+        }
+
+        // Sample code for building a localized ApplicationBar
+        private void BuildLocalizedApplicationBar()
+        {
+            // Set the page's ApplicationBar to a new instance of ApplicationBar.
+            ApplicationBar = new ApplicationBar();
+
+            // Create a new button and set the text value to the localized string from AppResources.
+            ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/questionmark.png", UriKind.Relative));
+            appBarButton.Text = AppResources.AppBarButtonText;
+            ApplicationBar.Buttons.Add(appBarButton);
+
+            // Create a new menu item with the localized string from AppResources.
+            ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+            ApplicationBar.MenuItems.Add(appBarMenuItem);
+
+            appBarButton.Click += appBarButton_Click;
+            appBarMenuItem.Click += appBarMenuItem_Click;
+        }
+
+        void appBarMenuItem_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page1.xaml", UriKind.Relative));
+        }
+
+        void appBarButton_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page1.xaml", UriKind.Relative));
+        }
+    }
+}
